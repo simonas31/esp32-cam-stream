810163b8a17c0fee30864d559a1821c4537f6113
simonas31 <babarskas.simonas@gmail.com>
2024-02-22 22:21:09 +0200

    remove directories

--- .pio/build/esp32cam/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32cam/FrameworkArduino/USB.cpp.o: \
- C:/Users/babar/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- C:/Users/babar/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/babar/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32cam/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32cam/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/babar/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/babar/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
