214d801eae5e3299e04a39a44ffbea9062fba116
simonas31 <babarskas.simonas@gmail.com>
2024-02-22 22:13:43 +0200

    edit .gitignore

---
+++ .pio/build/esp32cam/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32cam/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/babar/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/babar/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
