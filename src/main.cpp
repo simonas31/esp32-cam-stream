bc7b1df5dfd57a048b46a8195693f2dd66292c33
simonas31 <simonas.babarskas@ktu.edu>
2024-03-31 00:41:27 +0200

    optimize code and apply some simple mqtt requests to server

--- src/main.cpp
+++ src/main.cpp
@@ -79,7 +79,7 @@ void connectToMQTT()
   }
 }
 
-void sendPayload(String encodedImage, String jsonString, JsonDocument doc, int *current_image_number, bool last_image)
+void sendPayload(String encodedImage, String jsonString, JsonDocument doc, int &current_image_number, bool last_image)
 {
   if (!mqtt_client.connected())
   {
@@ -99,17 +99,16 @@ void sendPayload(String encodedImage, String jsonString, JsonDocument doc, int *
   encodedImage = base64::encode(fb->buf, fb->len);
   doc["image"] = "\"" + encodedImage + "\"";
   doc["file_name"] = "\"EW" + String(ESP.getEfuseMac()) + "\"";
+  doc["image_number"] = current_image_number;
   // find out if last message
   if (last_image)
   {
-    doc["last_message"] = true;
+    doc["last_image"] = true;
   }
   else
   {
-    doc["last_message"] = false;
+    doc["last_image"] = false;
   }
-
-  doc["image_number"] = current_image_number;
   current_image_number++;
 
   convertFromJson(doc, jsonString);
@@ -129,10 +128,10 @@ void burstImages_cb(void *pvParameters)
   // {
   while (millis() - currentTime <= 15000)
   {
-    sendPayload(encodedImage, jsonString, doc, &current_image_number, false);
+    sendPayload(encodedImage, jsonString, doc, current_image_number, false);
   }
   // add last frame send that it is the last and start creating video in python
-  sendPayload(encodedImage, jsonString, doc, &current_image_number, true);
+  sendPayload(encodedImage, jsonString, doc, current_image_number, true);
 
   delay(10);
   // }
@@ -179,95 +178,19 @@ esp_err_t initCamera()
   s->set_framesize(s, FRAMESIZE_VGA);
   Serial.println("camera init OK");
   return ESP_OK;
-};
-
-esp_err_t initCamera()
-{
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-
-  // parameters for image quality and size
-  config.frame_size = FRAMESIZE_XGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-  config.jpeg_quality = 15;          // 10-63 lower number means higher quality
-  config.fb_count = 2;
-
-  // Camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK)
-  {
-    Serial.printf("camera init FAIL: 0x%x", err);
-    delay(2000);
-    ESP.restart();
-  }
-  sensor_t *s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_VGA);
-  Serial.println("camera init OK");
-  return ESP_OK;
-};
+}
 
 void setup()
 {
   Serial.begin(115200);
   delay(1000);
 
-  // Configure the camera
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-
-  // Frame size options
-  // config.frame_size = FRAMESIZE_SXGA;
-  // config.frame_size = FRAMESIZE_UXGA;
-  // config.frame_size = FRAMESIZE_SVGA;
-  // config.frame_size = FRAMESIZE_QVGA;
-  config.frame_size = FRAMESIZE_VGA;
-  config.jpeg_quality = 15;
-  config.fb_count = 2; // on esp32 psram is enabled
-
   initCamera();
 
   Serial.println("Camera initialized successfully.");
-  WiFi.begin("TP-Link_4FD6", "53503794");
+  WiFi.begin("DIR-825-1dd3", "88169798");
 
   Serial.print("Connecting to Wi-Fi");
-  unsigned long ms = millis();
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
@@ -285,15 +208,27 @@ void setup()
   mqtt_client.setBufferSize(30000);
   connectToMQTT();
 
+  int current_image_number = 1;
+  Serial.println("Starting upload...");
+  String encodedImage, jsonString = "";
+  JsonDocument doc;
+  long int currentTime = millis();
+
+  while (millis() - currentTime <= 15000)
+  {
+    sendPayload(encodedImage, jsonString, doc, current_image_number, false);
+  }
+  // add last frame send that it is the last and start creating video in python
+  sendPayload(encodedImage, jsonString, doc, current_image_number, true);
   // core 0 bursts images to google cloud
-  xTaskCreatePinnedToCore(
-      burstImages_cb,
-      "burstImages",
-      15000,
-      NULL,
-      2,
-      &burstImages_t,
-      APP_CPU);
+  // xTaskCreatePinnedToCore(
+  //     burstImages_cb,
+  //     "burstImages",
+  //     15000,
+  //     NULL,
+  //     2,
+  //     &burstImages_t,
+  //     APP_CPU);
 }
 
 void loop()
